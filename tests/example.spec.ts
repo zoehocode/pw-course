a3c9166927e1735be6cea8699e879faa67553a8a
Zoe Nguyen <zoenguyen.contact@gmail.com>
2025-11-08 21:48:19 +0700

    homework

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,22 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("has title", async ({ page }) => {
+  await page.goto("https://material.playwrightvn.com/");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Tài liệu học automation test/);
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("get started link", async ({ page }) => {
+  await page.goto("https://material.playwrightvn.com/");
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page
+    .getByRole("link", { name: "Bài học 1: Register Page (có đủ các element)" })
+    .click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(
+    page.getByRole("heading", { name: "User Registration" })
+  ).toBeVisible();
 });
